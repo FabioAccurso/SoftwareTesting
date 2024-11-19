45d9cd42f3f23ed3795abd2919863eddae744bab
Fabio Accurso <oibaf15@gmail.com>
2024-11-19 19:40:37 +0100

    new class

---
+++ 3_Homework/Gamification/ClassiDaProvare/1_classi/HSLColor/src/ClassUnderTest/HSLColorFabioAccursoRandoopEasy.java
@@ -0,0 +1,51 @@
+package ClassUnderTest;
+
+/*Compila i campi "Nome" e "Cognome" con le informazioni richieste
+Nome: Fabio
+Cognome: Accurso
+Username: fa.accurso@studenti.unina.it
+UserID: 12
+Date: 18/11/2024
+*/
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.After;
+import org.junit.BeforeClass;
+import org.junit.AfterClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class HSLColorFabioAccursoRandoopEasy {
+  		
+	@BeforeClass
+	public static void setUpClass() {
+		// Eseguito una volta prima dell'inizio dei test nella classe
+		// Inizializza risorse condivise 
+		// o esegui altre operazioni di setup
+	}
+				
+	@AfterClass
+	public static void tearDownClass() {
+		// Eseguito una volta alla fine di tutti i test nella classe
+		// Effettua la pulizia delle risorse condivise 
+		// o esegui altre operazioni di teardown
+	}
+				
+	@Before
+	public void setUp() {
+		// Eseguito prima di ogni metodo di test
+		// Preparazione dei dati di input specifici per il test
+      	
+	}
+				
+	@After
+	public void tearDown() {
+		// Eseguito dopo ogni metodo di test
+		// Pulizia delle risorse o ripristino dello stato iniziale
+	}
+				
+}
+
+						
