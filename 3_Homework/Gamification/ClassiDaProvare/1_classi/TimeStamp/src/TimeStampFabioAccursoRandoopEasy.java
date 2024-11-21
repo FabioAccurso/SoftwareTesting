5a99f3104fc6955247edd6fdb6b79c7e03c6bbae
Fabio Accurso <oibaf15@gmail.com>
2024-11-21 20:06:41 +0100

    init last class

---
+++ 3_Homework/Gamification/ClassiDaProvare/1_classi/TimeStamp/src/TimeStampFabioAccursoRandoopEasy.java
@@ -0,0 +1,73 @@
+/*Compila i campi "Nome" e "Cognome" con le informazioni richieste
+Nome: Fabio
+Cognome: Accurso
+Username: fa.accurso@studenti.unina.it
+UserID: 12
+Date: 21/11/2024
+*/
+import static org.junit.Assert.*;
+
+import java.util.Calendar;
+import java.util.concurrent.ThreadPoolExecutor.DiscardOldestPolicy;
+
+import org.junit.Before;
+import org.junit.After;
+import org.junit.BeforeClass;
+import org.junit.AfterClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class TimeStampFabioAccursoRandoopEasy {
+	public static TimeStamp t;
+
+
+	@BeforeClass
+	public static void setUpClass() {
+		// Eseguito una volta prima dell'inizio dei test nella classe
+		// Inizializza risorse condivise 
+		// o esegui altre operazioni di setup
+	}
+				
+	@AfterClass
+	public static void tearDownClass() {
+		// Eseguito una volta alla fine di tutti i test nella classe
+		// Effettua la pulizia delle risorse condivise 
+		// o esegui altre operazioni di teardown
+	}
+				
+	@Before
+	public void setUp() {
+		// Eseguito prima di ogni metodo di test
+		// Preparazione dei dati di input specifici per il test
+      	
+	}
+				
+	@After
+	public void tearDown() {
+		// Eseguito dopo ogni metodo di test
+		// Pulizia delle risorse o ripristino dello stato iniziale
+	}
+				
+	//Test -------------------------------------------------------------------------------------------------------------------------------------------------------------------
+	//Test 1:
+	@Test
+	public void test() {
+		
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	/*
+	//Test :
+	@Test
+	public void test() {
+		
+	}
+	*/
+}
